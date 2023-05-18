27649ee615f0e164c65cdedc9cdb60e6040289c8
lmarques39 <63586956+lmarques39@users.noreply.github.com>
2023-05-18 13:56:45 +0100

    added features

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -1,6 +1,6 @@
-import PromptCard from './PromptCard';
+import PromptCard from "./PromptCard";
 
-const Profile = ({ name, desc, data, handleEdit, handleDelete}) => {
+const Profile = ({ name, desc, data, handleEdit, handleDelete }) => {
   return (
     <section className='w-full'>
       <h1 className='head_text text-left'>
@@ -9,17 +9,17 @@ const Profile = ({ name, desc, data, handleEdit, handleDelete}) => {
       <p className='desc text-left'>{desc}</p>
 
       <div className='mt-10 prompt_layout'>
-      {data.map((post) => (
-        <PromptCard 
-          key={post._id}
-          post={post}
-          handleEdit={() => handleEdit && handleEdit(post)}
-          handleDelete={() => handleDelete && handleDelete(post)}
-        />
-      ))}
-    </div>
+        {data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
